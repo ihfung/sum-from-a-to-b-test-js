efa4b942eff0cad0b8f86efde738944297e1f985
ihleung <ihfungleung@gmail.com>
2024-08-29 12:33:26 -0400

    add sum function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let result = 0;
+  //no for or while loops
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    result = fromN + sum(fromN + 1, toN);
+  }
+  return result;
+};
+
 
 module.exports = sum;
