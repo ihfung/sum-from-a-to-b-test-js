441479037df067710f99cfbc981da1b851aaf0b8
ihleung <ihfungleung@gmail.com>
2024-08-29 12:34:44 -0400

    add comments

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
   let result = 0;
   //no for or while loops
-  if (fromN === toN) {
+  if (fromN === toN) { //if the first number is the same as the second number then return the first number
     return fromN;
   } else {
-    result = fromN + sum(fromN + 1, toN);
+    result = fromN + sum(fromN + 1, toN); //else it would add the first number to the sum of the next number
   }
   return result;
 };
